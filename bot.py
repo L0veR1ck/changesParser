1bc9f39ce0534aeab1421c7e13ea1de856eda732
Lavr <lavr.rar@yandex.ru>
2022-08-04 01:32:15 +0500

    Добавил возможность отслеживать несколько url Добавил /status

--- bot.py
+++ bot.py
@@ -6,13 +6,27 @@ bot = telebot.TeleBot(token)
 users = {}
 with open('users.txt', 'r') as file:
     for i in file.readlines():
-        key, val = i.strip().split(';')
-        users[key] = val
+        input_list = list(i.strip().split(';'))
+        chat_id = input_list[0]
+        url_list = input_list[1:]
+        users[chat_id] = url_list
+print(users)
 
 
 @bot.message_handler(commands=['stop'])
 def stop(msg):
-    bot.send_message(msg.chat.id, 'Прекращаю отслеживать изменения')
+    bot.send_message(msg.chat.id, 'Прекращаю отслеживать изменения')  # допилить
+
+
+@bot.message_handler(commands=['status'])
+def list(msg):
+    try:
+        output_string = ''
+        for url in users[str(msg.chat.id)]:
+            output_string += url + '\n'
+        bot.send_message(msg.chat.id, output_string)
+    except:
+        bot.send_message(msg.chat.id, 'Ошибка: отслеживаемые сайты не найдены')
 
 
 @bot.message_handler(content_types=['text'])
@@ -21,11 +35,20 @@ def start(msg, res=False):
     try:
         requests.get(msg.text)
         url = msg.text
-        users[user] = url
+        if user in users:
+            users[user].append(url)
+        else:
+            users[user] = [url]
         with open('users.txt', 'w') as file:
-            for u in users:
-                file.write(u + ';' + users[u] + '\n')
-        bot.send_message(msg.chat.id, 'Начинаю отслеживать изменения.\nВведите /stop для отмены')
+            for chat_id in users:
+                output_string = str(chat_id)
+                for output_url in users[chat_id]:
+                    output_string += ';' + output_url
+                output_string += '\n'
+                file.write(output_string)
+
+        bot.send_message(msg.chat.id, f'Начинаю отслеживать изменения на {url}.\nВведите /stop для отмены\nВведите '
+                                      f'/list чтобы просмотреть список отслеживаемых сайтов')
     except:
         bot.send_message(msg.chat.id, 'Введите адрес сайта для отслеживания изменений')
 
--- main.py
+++ main.py
@@ -3,46 +3,57 @@ import hashlib
 import telebot
 import datetime
 
-
 token = '5493314841:AAH-rW6sOPXetnX18V9rYnPx1xWdfR8w09U'
 bot = telebot.TeleBot(token)
 
-users = {}
-with open('users.txt', 'r') as file:
-    for i in file.readlines():
-        key, val = i.strip().split(';')
-        users[key] = val
 
-hashes = {}
-with open('hashes.txt', 'r') as file:
-    for i in file.readlines():
-        key, val = i.strip().split(';')
-        hashes[key] = val
+def read_file(filename):
+    data = {}
+    with open(filename, 'r') as file:
+        for i in file.readlines():
+            data_list = list(i.strip().split(';'))
+            key = data_list[0]
+            val = data_list[1:]
+            data[key] = val
+    return data
 
 
-def main(user, url):
-    src = requests.get(url).text
-    time = datetime.datetime.now()
-    src_hash = hashlib.md5(src.encode()).hexdigest()
-    try:
-        src_hash_old = hashes[user]
-    except:
-        hashes[user] = src_hash
-        return
-    if src_hash != src_hash_old:
-        hashes[user] = src_hash
-        print(f'У {user} есть изменения') # и написать челу
-        bot.send_message(user, f'{time}\nПроведена проверка. На отслеживаемом сайте произошли изменения')
-    else:
-        print(f'У {user} нет изменений')
-        bot.send_message(user, f'{time}\nПроведена проверка. Изменения на отслеживаемом сайте отсутствуют')
+def track_changes(user, url_list, hashes):
+    if user not in hashes:
+        hashes[user] = []
+        for url in url_list:
+            src = requests.get(url).text
+            src_hash = hashlib.md5(src.encode()).hexdigest()
+            hashes[user].append(src_hash)
+
+    for i in range(len(url_list)):
+        url = url_list[i]
+        src = requests.get(url).text
+        time, milliseconds = str(datetime.datetime.now()).split('.')
+        src_hash = hashlib.md5(src.encode()).hexdigest()
+        src_hash_old = hashes[user][i]
+
+        if src_hash != src_hash_old:
+            hashes[user][i] = src_hash
+            print(f'У {user} есть изменения на {url}')  # и написать челу
+            bot.send_message(user, f'{time}\nПроведена проверка. На {url} произошли изменения')
+        else:
+            print(f'У {user} нет изменений на {url}')
+            bot.send_message(user, f'{time}\nПроведена проверка. Изменения на {url} отсутствуют')
 
 
 if __name__ == "__main__":
+    users = read_file('users.txt')
+    hashes = read_file('hashes.txt')
+
     for user in users:
-        url = users[user]
-        main(user, url)
+        url_list = users[user]
+        track_changes(user, url_list, hashes)
 
     with open('hashes.txt', 'w') as file:
-        for u in hashes:
-            file.write(u + ';' + hashes[u] + '\n')
+        for chat_id in hashes:
+            output_string = chat_id
+            for cur_hash in hashes[chat_id]:
+                output_string += ';' + cur_hash
+            output_string += '\n'
+            file.write(output_string)
