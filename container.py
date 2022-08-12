8f78c7d1313a8f2e452ae1c54dc4aa5214d9b401
Lavr <lavr.rar@yandex.ru>
2022-08-12 13:35:22 +0500

    Создал класс Container

--- container.py
+++ container.py
@@ -1,37 +1,26 @@
-class Container:
+class Repository:
     def __init__(self):
         self.storage = {}
 
-    def create(self, table_name, table_data=None):
-        if data is None:
-            table_data = {}
-        if table_name in self.storage:
-            print(f'Name \'{table_name}\' is already taken')
-            return
-        self.storage[table_name] = table_data
+    def create(self, data: dict):
+        self.storage.update(data)
 
-    def read(self, table_name=None):
-        if table_name is None:
+    def read(self, elem=None):
+        if elem is None:
             return self.storage
-        try:
-            return self.storage[table_name]
-        except:  # почему подчеркивает? исправить на finally помогает, но вроде логичнее except
-            print(f'Table \'{table_name}\' does not exist')
-            return
+        return self.storage[elem]
 
-    def delete(self, table_name):
-        self.storage.pop(table_name)
+    def delete(self, elem):
+        return self.storage.pop(elem)
 
-    def update(self, table_name, new_data):  # наверное надо как то умнее :)
-        self.delete(table_name)
-        self.create(table_name, new_data)
+    def update(self, new_data):
+        self.storage.update(new_data)
 
 
-data = Container()
-users = {'l0ver1ck': 228, 'snoward': 322}
-kekers = ['keker1', 'keker2']
-data.create('users', users)
-data.create('kekers', kekers)
-print(data.read())
-data.create('users', ['lolik', 'nolik'])
-data.read('abdul')
+hashes = Repository()
+lavrHash = {'id': 123, 'value': 'DKFDHDKJFHD'}
+hashes.create(lavrHash)
+print(hashes.read())
+lavrHash = {'id': 123, 'value': 'ZZZZZZZZZ'}
+hashes.update(lavrHash)
+print(hashes.read())
