49c31c9259cd9299808d88557b59c4ce940c2357
Lavr <lavr.rar@yandex.ru>
2022-08-16 22:31:19 +0500

    подключил SQLiteRepository

--- bot.py
+++ bot.py
@@ -1,56 +1,70 @@
 import telebot
 import requests
+from repository import SQLiteRepository
+from main import calc_hash
 
 token = '5493314841:AAH-rW6sOPXetnX18V9rYnPx1xWdfR8w09U'
 bot = telebot.TeleBot(token)
-users = {}
-with open('users.txt', 'r') as file:
-    for i in file.readlines():
-        input_list = list(i.strip().split(';'))
-        chat_id = input_list[0]
-        url_list = input_list[1:]
-        users[chat_id] = url_list
-print(users)
 
 
 @bot.message_handler(commands=['stop'])
 def stop(msg):
-    bot.send_message(msg.chat.id, 'Прекращаю отслеживать изменения')  # допилить
+    global delete_flag
+    bot.send_message(msg.chat.id, 'Введите сайт для прекращения отслеживания изменений')
+    delete_flag = True
 
 
 @bot.message_handler(commands=['status'])
 def status(msg):
+    chat_id = str(msg.chat.id)
     try:
         output_string = ''
-        for url in users[str(msg.chat.id)]:
-            output_string += url + '\n'
+        urls = hashes.read_all()
+
+        for user_url, src_hash in urls:
+            user, url = user_url.split('_')
+            if user == chat_id:
+                output_string += url + '\n'
+
         bot.send_message(msg.chat.id, output_string)
     except:
         bot.send_message(msg.chat.id, 'Ошибка: отслеживаемые сайты не найдены')
 
 
 @bot.message_handler(content_types=['text'])
-def start(msg, res=False):
-    user = str(msg.chat.id)
-    try:
-        requests.get(msg.text)
-        url = msg.text
-        if user in users:
-            users[user].append(url)
-        else:
-            users[user] = [url]
-        with open('users.txt', 'w') as file:
-            for chat_id in users:
-                output_string = str(chat_id)
-                for output_url in users[chat_id]:
-                    output_string += ';' + output_url
-                output_string += '\n'
-                file.write(output_string)
+def start(msg):
+    chat_id = str(msg.chat.id)
 
-        bot.send_message(msg.chat.id, f'Начинаю отслеживать изменения на {url}.\nВведите /stop для отмены\nВведите '
-                                      f'/status чтобы просмотреть список отслеживаемых сайтов')
+    if delete_flag:
+        url = msg.text
+        if len(url) == 0:
+            return
+        if url[-1] != '/':
+            url += '/'
+        key = chat_id + '_' + url
+        hashes.delete(key)
+        bot.send_message(msg.chat.id, f'Вы прекратили отслеживать изменения на {url}')
+        return
+
+    try:
+        src = requests.get(msg.text).text
+        src_hash = calc_hash(src)
+
+        url = msg.text
+        if url[-1] != '/':
+            url += '/'
+
+        key = chat_id + '_' + url
+        hashes.create(key, src_hash)
+
+        bot.send_message(msg.chat.id, f'Начинаю отслеживать изменения на {url}.\nВведите /stop для отмены\n'
+                                      f'Введите /status чтобы просмотреть список отслеживаемых сайтов')
     except:
         bot.send_message(msg.chat.id, 'Введите адрес сайта для отслеживания изменений')
 
 
-bot.infinity_polling()
+if __name__ == '__main__':
+    delete_flag = False
+    hashes = SQLiteRepository('hashes', 'chat_id_url', 'hash')
+
+    bot.infinity_polling()
